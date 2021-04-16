343ca6edf0d3e4fd5b643c7efb54b46ec3858f30
Carlos Eduardo Pereira Alves <carlos.alves@meta.com.br>
2021-04-15 22:06:41 -0300

    refactor: end config docker and connect to application

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl:
+    process.env.MONGO_URL ||
+    `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CONTAINER}/${process.env.MONGO_DB}?authSource=admin`,
   port: process.env.PORT || 3333,
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,14 @@
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
-import app from './config/app'
 
-app.listen(env.port, () =>
-  console.log(`Server listening on http://localhost:${env.port}`)
-)
+MongoHelper.connect(env.mongoUrl)
+
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () =>
+      console.log(`Server listening on http://localhost:${env.port}`)
+    )
+  })
+  .catch((error) => {
+    console.log('\x1b[31m', '[API] Error on connect DB', error)
+  })
